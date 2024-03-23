2f6d87b36777d3ab24b4483080c453435d578bc5
karloshyadav <ykarlosh@gmail.com>
2024-03-23 16:11:32 +0545

    My Project

---
+++ src/components/Mainbody.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export default function Mainbody() {
+    return (
+        <>
+            <div className='row'>
+                <div className='col-sm-8'>
+                <div className='container d-flex flex-column justify-content-center align-items-center mt-5 '>
+                    <img src='./whastappdp.jpg' style={{height:'100vh',width:'auto'}}/>
+                </div>
+                </div>
+                <div className='col-sm-4'>
+                    <div className='container d-flex flex-column justify-content-center align-items-center mt-5 '>
+                        <div className='row '>
+                            <div className='col-sm-6'>
+                                <div class="card ">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="card-link">Card link</a>
+                                        <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div class="card ">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="card-link">Card link</a>
+                                        <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div class="card mt-4">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="card-link">Card link</a>
+                                        <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-sm-6'>
+                                <div class="card mt-4">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="card-link">Card link</a>
+                                        <a href="#" class="card-link">Another link</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
